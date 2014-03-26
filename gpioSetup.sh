babcf0a268edb30718b3aae95428650f9b2062f3
alx-s <saunier.alexandre@yahoo.com>
2014-03-26 14:52:59 +0100

    nearly there

--- gpioSetup.sh
+++ gpioSetup.sh
@@ -27,4 +27,22 @@ echo out > /sys/class/gpio/gpio8/direction
 echo 4 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio4/direction
 
+echo 14 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio14/direction
+
+echo 15 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio15/direction
+
+echo 18 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio18/direction
+
+echo 23 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio23/direction
+
+echo 24 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio24/direction
+
+echo 25 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio25/direction
+
 echo "setup done"
